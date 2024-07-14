b6b78f86bcad1c6c89ca1a7d5687c4612b42934b
Kimberly Schwartz <kimberlyschwartz@kimberlys-mbp.myfiosgateway.com>
2024-07-14 13:31:17 -0400

    refactored some into commands

--- cypress/e2e/end2End.cy.js
+++ cypress/e2e/end2End.cy.js
@@ -9,83 +9,66 @@ describe('Tests to make sure Scale is functioning as expected', () => {
     // visit site
     cy.visit('http://sdetchallenge.fetch.com/');
     // input 0 and 8 in the bowls
-    cy.leftBowl('0');
-    cy.rightBowl('8');
-    // click weigh
-    cy.get('[id="weigh"]').click();
-    // average wait for results to be produced, ideally would do this off of a network call instead but there are no discernible network calls
-    cy.wait(3000);
-    // check result for < or > and click correct coin in accordance
+    cy.weighBars('0', '8');
+    // check result for < or > and click correct bar in accordance
     cy.get('.result')
       .invoke('text')
       .then((text1) => {
         if (text1.includes('<')) {
-          cy.get('[id="coin_0"]').click();
+          cy.get(`[id="coin_${0}"]`).click();
           // output number of weighing
-          cy.log('Bar 0 is fake!');
+          cy.fakeBar('0');
         } else if (text1.includes('>')) {
           cy.get('[id="coin_8"]').click();
           // output number of weighing
-          cy.log('Bar 8 is fake!');
+          cy.fakeBar('8');
         } else {
           // if bars are equal, hit reset and input 1 and 7 in bowls and try again
-          cy.get('[id="reset"]').contains('Reset').click();
-          cy.leftBowl('1');
-          cy.rightBowl('7');
-          cy.get('[id="weigh"]').click();
-          cy.wait(3000);
+          cy.resetWeigh('1', '7');
           cy.get('.result')
             .invoke('text')
             .then((text2) => {
               if (text2.includes('<')) {
                 cy.get('[id="coin_1"]').click();
                 // output number of weighing
-                cy.log('Bar 1 is fake!');
+                cy.fakeBar('1');
               } else if (text2.includes('>')) {
                 cy.get('[id="coin_7"]').click();
                 // output number of weighing
-                cy.log('Bar 7 is fake!');
+                cy.fakeBar('7');
               } else {
                 // if bars are equal, hit reset and input 2 and 6 in bowls and try again
-                cy.get('[id="reset"]').contains('Reset').click();
-                cy.leftBowl('2');
-                cy.rightBowl('6');
-                cy.get('[id="weigh"]').click();
-                cy.wait(3000);
+                cy.resetWeigh('2', '6');
                 cy.get('.result')
                   .invoke('text')
                   .then((text3) => {
                     if (text3.includes('<')) {
                       cy.get('[id="coin_2"]').click();
                       // output number of weighing
-                      cy.log('Bar 2 is fake!');
+                      cy.fakeBar('2');
                     } else if (text3.includes('>')) {
                       cy.get('[id="coin_6"]').click();
                       // output number of weighing
-                      cy.log('Bar 6 is fake!');
+                      cy.fakeBar('6');
                     } else {
                       // if bars are equal, hit reset and input 3 and 5 in bowls and try again
-                      cy.get('[id="reset"]').contains('Reset').click();
-                      cy.leftBowl('3');
-                      cy.rightBowl('5');
-                      cy.get('[id="weigh"]').click();
-                      cy.wait(3000);
+                      cy.resetWeigh('3', '5');
                       cy.get('.result')
                         .invoke('text')
                         .then((text4) => {
                           if (text4.includes('<')) {
                             cy.get('[id="coin_3"]').click();
                             // output number of weighing
-                            cy.log('Bar 3 is fake!');
+                            cy.fakeBar('3');
                           } else if (text4.includes('>')) {
                             cy.get('[id="coin_5"]').click();
                             // output number of weighing
-                            cy.log('Bar 5 is fake!');
+                            cy.fakeBar('5');
                           } else {
                             // if none of the previous weighings produced the fake bar, the fake one is 4
                             cy.get('[id="coin_4"]').click();
                             // output number of weighing
-                            cy.log('Bar 4 is fake!');
+                            cy.fakeBar('4');
                           }
                         });
                     }
@@ -94,11 +77,9 @@ describe('Tests to make sure Scale is functioning as expected', () => {
             });
         }
       });
-    // verify alert produces expected success message
+    // verifies alert produces expected success message as well as outputs alert
     cy.on('window:alert', (str) => {
       expect(str).to.equal('Yay! You find it!');
-      //Output alert message, number of weighing and list of weighing made
-      //cy.log();
       // output list of weighings made
       cy.get('.game-info')
         .invoke('text')
